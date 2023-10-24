cde14e11f7e556e735e09675ae54e4917dd006d1
Federicoorcajo <fedeorcajo97@outlook.com>
2023-10-24 20:51:32 +0200

    CONSEGNA COMPITO

---
+++ index.js
@@ -0,0 +1,8 @@
+// Domanda 1: Cos'è un algoritmo?
+// Un algoritmo è una serie di calcoli preimpostati per fare analisi delle tue mosse e quelle degli utenti.
+
+// Domanda 2: Cos'è una variabile?
+// Una variabile è un oggetto che si utiliza per taggare gli elementi, cioe un qualcosa che utilizzi per poter richiamare gli elementi. Esempio: Creo una cartella sul mio PC con il nome "FOTO DUBAI", e il fatto che gli ho dato un nome vuol dire che poi sul "Cercare in DOCUMENTI" posso cercare con quel nome e ritrovarla. Una delle funzioni delle variabili è questa porter assegnare un nome a un qualcosa.   
+
+// Domanda 3: Undefined e null sono la stessa cosa?
+// Secondo me no, perche UNDEFINED (Non definito) ti dice che il risultato non è definito, invece NULL (Niente) ti dice che è niente, perciò intendo che gia è stato definito ed è NIENTE.
